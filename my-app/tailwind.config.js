6ffea929194aad0230c5e0325ece8cc10b2ee11a
nullVallue <weijiant02@gmail.com>
2024-05-08 11:17:27 +0800

    Update tailwind.config.js

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -11,21 +11,48 @@ module.exports = {
     // colors: {},
     extend: {
       colors: {
+        // 'bodybg-color' : {
+        //   DEFAULT: Palette.coffeePalette['brown-yellow'][50],
+        // },
+        // 'navbg-color' : {
+        //   DEFAULT: Palette.coffeePalette['brown-yellow'][50],
+        // },
+        // 'navmenubg-color' : {
+        //   DEFAULT: Palette.coffeePalette['brown-yellow'][50],
+        // },
+        // 'navtext-color' : {
+        //   DEFAULT: Palette.coffeePalette['dark-brown'],
+        // },
+        // 'navtext-hover-color' : {
+        //   DEFAULT: Palette.coffeePalette['brown'],
+        // }
         'bodybg-color' : {
-          DEFAULT: Palette.coffeePalette['brown-yellow'][50],
+          DEFAULT: Palette.monoBluePalette['white'],
+        },
+        'bodybg-gradientstop-color' : {
+          DEFAULT: Palette.monoBluePalette['white'],
         },
         'navbg-color' : {
-          DEFAULT: Palette.coffeePalette['brown-yellow'][50],
+          DEFAULT: Palette.monoBluePalette['white'],
         },
         'navmenubg-color' : {
-          DEFAULT: Palette.coffeePalette['brown-yellow'][50],
+          DEFAULT: Palette.monoBluePalette['white'],
         },
         'navtext-color' : {
-          DEFAULT: Palette.coffeePalette['dark-brown'],
+          DEFAULT: Palette.monoBluePalette['dark-blue'][500],
         },
+        // 'navmenuicon-color' : {
+        //   DEFAULT: Palette.monoBluePalette['white'],
+        // },
         'navtext-hover-color' : {
-          DEFAULT: Palette.coffeePalette['brown'],
-        }
+          DEFAULT: Palette.monoBluePalette['dark-blue'][300],
+        },
+        'home-name-color' : {
+          DEFAULT: Palette.monoBluePalette['accent-blue'][500],
+        },
+        'home-rolesbg-color' : {
+          DEFAULT: Palette.monoBluePalette['dark-blue'][400],
+        },
       },
       boxShadow: {
         'nav-shadow' : '0 10px 20px 0 rgb(27 21 22 / 0.05)',
