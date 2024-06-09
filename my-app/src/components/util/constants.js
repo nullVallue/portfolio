fbb2218cd56e105023d5c08b7808e22fd239f275
nullVallue <weijiant02@gmail.com>
2024-06-09 12:24:53 +0800

    Add : new constants

--- my-app/src/components/util/constants.js
+++ my-app/src/components/util/constants.js
@@ -1,15 +1,37 @@
 
-// // Personal Info
-// export const myname = "Wei Jian";
-// export const myfullname = "Wei Jian Tan";
-// export const myposition = "Fullstack Developer";
+/**------------------------------------------------------------------------
+ *                           Personal Info
+ *------------------------------------------------------------------------**/
+export const myname = "Wei Jian";
+export const myfullname = "Wei Jian Tan";
+export const myposition = "Fullstack Developer";
 
-export const myname = "Placeholder Name";
-export const myfullname = "Placeholder Fullname";
-export const myposition = "Placeholder Pos";
+// export const myname = "Placeholder Name";
+// export const myfullname = "Placeholder Fullname";
+// export const myposition = "Placeholder Pos";
 
-// Links
+/**------------------------------------------------------------------------
+ *                           Links
+ *------------------------------------------------------------------------**/
 export const githubLink = "https://github.com/nullVallue";
 export const linkedInLink = "https://www.linkedin.com/in/wei-jian-tan/";
 export const emailLink = "mailto:weijiant02@gmail.com";
 export const instagramLink = "https://www.instagram.com/wei_jiantan";
+
+/**------------------------------------------------------------------------
+ *                          About Me 
+ *------------------------------------------------------------------------**/
+export const aboutMeP =  `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent cursus quam urna. Nulla eu mattis magna, a scelerisque purus. Nulla in leo fringilla, tempus metus tempus, commodo nibh. Curabitur at tortor eget metus feugiat dignissim. Sed vel nisl augue. Curabitur lacinia sed quam a suscipit. Aenean posuere nulla ac efficitur bibendum. In mattis non est vitae lobortis. In vehicula sed sapien non lacinia. 
+`;
+
+export const aboutMePic = require("../../assets/aboutMe/aboutMe1.JPG");
+
+/**------------------------------------------------------------------------
+ *                           Skills
+ *------------------------------------------------------------------------**/
+
+const reactLogo = require("../../assets/skillLogos/reactLogo.png");
+
+
+export const frontEndLogos = [reactLogo, reactLogo, reactLogo, reactLogo, reactLogo];
