e353c1528695aec5f928f9287f2fda0ed46fd594
nullVallue <weijiant02@gmail.com>
2024-06-17 01:19:09 +0800

    Update : add retrieval of card details

--- my-app/src/components/page-components/home/projects/Projects.js
+++ my-app/src/components/page-components/home/projects/Projects.js
@@ -1,11 +1,12 @@
-
+import ImgCard from "../../../card/ImgCard";
+import Pill from "../../../small-components/pill/base/Pill";
+import { projectsArr } from "../../../util/constants";
 
 export default function Projects(props){
 
 
     let containerClass = `
         w-screen
-        h-screen
         bg-home-projects-bgcolor 
         py-28
     `;
@@ -19,10 +20,80 @@ export default function Projects(props){
         w-screen
     `;
 
+    let cardContainer = `
+        w-screen
+        flex
+        justify-evenly
+        mt-10
+    `;
+
+    let card=`
+        xl:w-3/12
+        md:w-1/2
+        sm:w-9/12
+        w-10/12
+    `;
+
+    let pillContainer = `
+        gap-2
+        mt-3
+        flex
+    `;
+
+    function getProjects(arr){
+        let elementArr = [];
+
+        arr.forEach(p => {
+            let pillElements = [];
+
+            p.skills.forEach(e => {
+                pillElements.push(
+                    <Pill
+                        bgColor={e.mainColor}
+                        textColor={e.textColor}
+                    >
+                        {
+                            "#" + e.name
+                        }
+                    </Pill>
+                );
+            });
+
+
+            elementArr.push(
+                <ImgCard
+                    className={card}
+                    title={p.title}
+                    src={p.cardThumbnail}
+                    pointer={true}
+                    href={p.projectLink}
+                    linkText="Checkout The Project"
+                >
+                    {
+                        p.description
+                    }
+                    <div className={pillContainer}>
+                        {
+                            pillElements
+                        }
+                    </div>
+                </ImgCard>
+            );
+        });
+
+        return elementArr;
+    }
+
     return (
         <>
             <div id="projectsSection" className={containerClass}>
                 <h2 className={title}>Projects</h2>
+
+                <div className={cardContainer}>
+                    {
+                        getProjects(projectsArr)
+                    }
+                </div>
 
             </div>
         </>
