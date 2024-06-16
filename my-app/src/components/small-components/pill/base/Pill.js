5995d64b3c27f60753eb1ff392562083de9f70e3
nullVallue <weijiant02@gmail.com>
2024-06-17 01:16:38 +0800

    Add : Pill UI component

---
+++ my-app/src/components/small-components/pill/base/Pill.js
@@ -0,0 +1,60 @@
+
+
+export default function Pill(props){
+    let container = `
+        px-2
+        py-1
+        w-min
+        h-min
+        rounded-full
+        flex
+        justify-center
+        content-center
+    `;
+
+    let containerStyle = () => {
+        let obj = {}
+
+        if(props.bgColor){
+            obj.backgroundColor = props.bgColor;
+        }
+
+        return obj
+    }
+
+    let textStyle = () => {
+        let obj = {}
+
+        if(props.textColor){
+            obj.color = props.textColor;
+        }
+
+        return obj
+    }
+
+    let text=`
+        align-middle
+        font-bold
+        text-xs
+        p-0
+    `;
+
+
+    return (
+        <>
+            <div 
+                className={container} 
+                style={containerStyle()}
+            >
+                <span
+                    style={textStyle()}
+                    className={text}
+                >
+                    {
+                        props.children
+                    }
+                </span>
+            </div>
+        </>
+    );
+}
