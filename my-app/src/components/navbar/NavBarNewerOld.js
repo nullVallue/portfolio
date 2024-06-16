01feb3eb0298ed20ce7bd7e85c2f176ea909b578
nullVallue <weijiant02@gmail.com>
2024-06-17 03:32:19 +0800

    Add : backup old navbar file

---
+++ my-app/src/components/navbar/NavBarNewerOld.js
@@ -0,0 +1,311 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
+import { logoText } from "../util/constants";
+import {Squeeze as MenuIcon } from "hamburger-react";
+import Palette from "../util/theme/Palette";
+import { useEffect, useState } from "react"
+
+export default function NavBar(props){
+    const { isHome, isAboutMe, isProjects, isMilestones, isContactMe } = props;
+    const [isOpen, setOpen] = useState(false);
+
+    // follow tailwindcss
+    // nav bar class
+    let navClassStyle =  `
+        bg-navbg-color/95
+        fixed
+        w-screen
+        py-5 
+        px-5 
+        flex
+        flex-wrap
+        items-center
+        justify-between
+        z-50
+
+
+    `;
+
+        // transition
+        // duration-400
+        // ease-out
+        // -translate-y-12
+        // hover:-translate-y-0
+
+    let navLinkContainer = `
+        flex
+        items-center
+        justify-end
+
+        max-lg:hidden
+    `;
+
+    let hamburgContainer=`
+        lg:hidden
+    `;
+
+    let iconStyle = `
+    `;
+
+    let navLinkClass = `
+        transition-all
+        ease-in-out
+        duration-200
+
+
+
+        font-sans
+        font-black 
+        text-navtext-color
+        text-2xl
+        tracking-wider
+        mx-3
+        px-2
+        group
+
+
+    `;
+
+    let navLinkNoTitleClass=`
+
+        md:text-8xl
+        sm:text-7xl
+        text-6xl
+
+        text-nowrap
+        w-min
+    `;
+
+        // border
+        // border-solid
+        // border-2
+        // border-navtext-color
+        // rounded-lg
+
+    let logoLinkClass = `
+        font-mono
+        text-lg
+    `;
+
+    let spanUnderlineClass = `
+
+
+        block 
+        max-w-0 
+        group-hover:max-w-full 
+        transition-all 
+        duration-350 
+        h-1 
+        bg-navtextbg-hover-color
+
+        max-lg:h-5
+    `;
+
+    let sidebarDiv = `
+        w-screen
+        h-screen
+        fixed
+        z-40
+        pt-28
+        p-8
+        bg-navbg-color
+        lg:hidden
+
+        flex
+        flex-col
+    `;
+
+    return(
+        <>
+
+            <motion.div 
+                key="navBarKey"
+                className={`${navClassStyle}`}
+                initial={{
+                    opacity: 0,
+                    y: "-100%"
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                        duration: 0.25,
+                        ease: "easeInOut",
+                    }
+                }}
+                exit={{
+                    opacity: 0,
+                    y: "-100%"
+                }} 
+            >
+
+                <a href="#top" className={navLinkClass + logoLinkClass} onClick={()=>{setOpen(false)}}>
+                    {logoText}
+                </a>
+
+
+                <div className={navLinkContainer}>
+
+                    {/* <a href="#top" className={navLinkClass}>
+                        Home
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a> */}
+
+                    <a href="#aboutMeSection" className={navLinkClass}>
+                        About Me
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>
+                    <a href="#projectsSection" className={navLinkClass}>
+                        Projects
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>
+                    <a href="#milestonesSection" className={navLinkClass}>
+                        Milestones
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>
+                    <a href="#contactMeSection" className={navLinkClass}>
+                        Contact Me
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>
+
+                </div>
+
+                <div className={hamburgContainer}>
+                    <MenuIcon toggled={isOpen} toggle={setOpen} className={iconStyle} color={Palette.monoBluePalette['dark-blue'][500]} size={30} duration={0.2}/>
+                </div>
+
+
+
+
+                {/* {
+                    isHome?(
+                    <a href="#top" className={navLinkClass}>
+                        Home
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>):
+                    (<NavLink to="/" className={navLinkClass}>
+                        Home
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </NavLink>)
+                }
+                {
+                    isAboutMe?
+                    (<a href="#top" className={navLinkClass}>
+                        About Me
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>):
+                    (<NavLink to="/AboutMe" className={navLinkClass}>
+                        About Me
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </NavLink>)
+                }
+                {
+                    isProjects?
+                    (<a href="#top" className={navLinkClass}>
+                        Projects
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>):
+                    (<NavLink to="/Projects" className={navLinkClass}>
+                        Projects
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </NavLink>)
+                }
+                {
+                    isMilestones?
+                    (<a href="#top" className={navLinkClass}>
+                        Milestones
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>):
+                    (<NavLink to="/Milestones" className={navLinkClass}>
+                        Milestones
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </NavLink>)
+                }
+                {
+                    isContactMe?
+                    (<a href="#top" className={navLinkClass}>
+                        Contact Me
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </a>):
+                    (<NavLink to="/ContactMe" className={navLinkClass}>
+                        Contact Me
+                        <span className={spanUnderlineClass}>
+                        </span>
+                    </NavLink>)
+                } */}
+                
+            </motion.div>
+
+            <AnimatePresence>
+                {
+                    isOpen && (
+                        <motion.div
+                            className={sidebarDiv}
+                            initial={{
+                                opacity: 0,
+                                x: "100%"
+                            }}
+                            animate={{
+                                opacity: 1,
+                                x: 0,
+                                transition: {
+                                    duration: 0.25,
+                                    ease: "easeInOut",
+                                }
+                            }}
+                            exit={{
+                                opacity: 0,
+                                x: "100%"
+                            }} 
+                        >
+
+                            <a href="#aboutMeSection" className={navLinkClass  + navLinkNoTitleClass} onClick={()=>{setOpen(false)}}>
+                                About Me
+                                <span className={spanUnderlineClass}>
+                                </span>
+                            </a>
+                            <a href="#projectsSection" className={navLinkClass + navLinkNoTitleClass} onClick={()=>{setOpen(false)}}>
+                                Projects
+                                <span className={spanUnderlineClass}>
+                                </span>
+                            </a>
+                            <a href="#milestonesSection" className={navLinkClass + navLinkNoTitleClass} onClick={()=>{setOpen(false)}}>
+                                Milestones
+                                <span className={spanUnderlineClass}>
+                                </span>
+                            </a>
+                            <a href="#contactMeSection" className={navLinkClass + navLinkNoTitleClass} onClick={()=>{setOpen(false)}}>
+                                Contact Me
+                                <span className={spanUnderlineClass}>
+                                </span>
+                            </a>
+
+
+
+                        </motion.div>
+                    )
+                }
+
+            </AnimatePresence>
+
+
+
+
+        </>
+    );
+}
+
