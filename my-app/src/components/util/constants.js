70fbe1e6c06da65dfa71429c97ef9b9a424463a3
nullVallue <weijiant02@gmail.com>
2024-06-16 19:43:28 +0800

    Update: Add logo text in constants

--- my-app/src/components/util/constants.js
+++ my-app/src/components/util/constants.js
@@ -11,6 +11,11 @@ export const myposition = "Fullstack Developer";
 // export const myposition = "Placeholder Pos";
 
 /**------------------------------------------------------------------------
+ *                           Navbar
+ *------------------------------------------------------------------------**/
+export const logoText = "weijian_";
+
+/**------------------------------------------------------------------------
  *                           Links
  *------------------------------------------------------------------------**/
 export const githubLink = "https://github.com/nullVallue";
@@ -31,7 +36,94 @@ export const aboutMePic = require("../../assets/aboutMe/aboutMe1.JPG");
  *                           Skills
  *------------------------------------------------------------------------**/
 
+// front end
 const reactLogo = require("../../assets/skillLogos/reactLogo.png");
+const cssLogo = require("../../assets/skillLogos/cssLogo.png");
+const flutterLogo = require("../../assets/skillLogos/flutterLogo.png");
+const htmlLogo = require("../../assets/skillLogos/htmlLogo.png");
+const tailwindLogo = require("../../assets/skillLogos/tailwindLogo.png");
+const bootstrapLogo = require("../../assets/skillLogos/bootstrapLogo.png");
+
+// back end
+const dartLogo = require("../../assets/skillLogos/dartLogo.png");
+const firebaseLogo = require("../../assets/skillLogos/firebaseLogo.png");
+const javascriptLogo = require("../../assets/skillLogos/javascriptLogo.png");
+const mongoLogo = require("../../assets/skillLogos/mongoLogo.png");
+const nextjsLogo = require("../../assets/skillLogos/nextjsLogo.png");
+const pythonLogo = require("../../assets/skillLogos/pythonLogo.png");
+
+// others
+const figmaLogo = require("../../assets/skillLogos/figmaLogo.png");
+const gitLogo= require("../../assets/skillLogos/gitLogo.png");
+const premiereLogo = require("../../assets/skillLogos/premiereLogo.png");
 
 
-export const frontEndLogos = [reactLogo, reactLogo, reactLogo, reactLogo, reactLogo];
+
+export const frontEndLogos = [
+    {
+        logo: flutterLogo,
+        name: "Flutter"
+    },
+    {
+        logo: reactLogo,
+        name: "React"
+    },
+    {
+        logo: tailwindLogo,
+        name: "Tailwind"
+    },
+    {
+        logo: bootstrapLogo,
+        name: "Bootstrap"
+    },
+    {
+        logo: htmlLogo,
+        name: "HTML"
+    },
+    {
+        logo: cssLogo,
+        name: "CSS"
+    },
+];
+
+export const backEndLogos = [
+    {
+        logo: javascriptLogo,
+        name: "Javscript"
+    },
+    {
+        logo: dartLogo,
+        name: "Dart"
+    },
+    {
+        logo: pythonLogo,
+        name: "Python"
+    },
+    {
+        logo: nextjsLogo,
+        name: "NextJS"
+    },
+    {
+        logo: mongoLogo,
+        name: "MongoDB"
+    },
+    {
+        logo: firebaseLogo,
+        name: "Firebase"
+    },
+];
+
+export const othersLogos = [
+    {
+        logo: figmaLogo,
+        name: "Figma"
+    },
+    {
+        logo: gitLogo,
+        name: "Git"
+    },
+    {
+        logo: premiereLogo,
+        name: "Premiere"
+    },
+];
