b92f4390962b2433d4b8ee88db2ea37de9e29645
nullVallue <weijiant02@gmail.com>
2024-07-31 22:05:19 +0800

    Style : Add simple page loaded animation
    Scope :
    - Layout.js
    - Loader.js

--- my-app/src/components/Layout.js
+++ my-app/src/components/Layout.js
@@ -1,8 +1,12 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./util/Loader";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Layout(){
+
+    // const navigation = useNavigation();
+
     return(
         <>
 
@@ -10,6 +14,35 @@ export default function Layout(){
             <div className="h-screen w-screen">
                 <main>
                     <Suspense fallback={<Loader />}>
+                        {/* {
+                            navigation.state === "loading"? 
+                            <Loader />
+                            : 
+                            <Outlet /> 
+                        } */}
+                        <AnimatePresence>
+                            <motion.div
+                                initial={{x: 0}} 
+                                animate={{
+                                    x: "-100%",
+                                    transition: {
+                                        duration: 1,
+                                        ease: "easeInOut",
+                                        delay: 0.2,
+                                    }
+                                }}
+                                exit={{ x: 0}}
+                                className="
+                                    absolute
+                                    inset-0
+                                    w-screen
+                                    h-screen
+                                    bg-loaderbg-color
+                                    z-50
+                                "
+                            >
+                            </motion.div>
+                        </AnimatePresence>
                         <Outlet />
                     </Suspense>
                 </main>
--- my-app/src/components/util/Loader.js
+++ my-app/src/components/util/Loader.js
@@ -1,4 +1,29 @@
+import { squircle  } from 'ldrs';
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Loader(){
-    return <div>Loading..</div>
+
+    squircle.register();
+
+    return (
+        <>
+                <div 
+                    className="
+                        bg-loaderbg-color
+                        h-screen
+                        w-screen
+                        flex
+                        justify-center
+                        items-center
+                    "
+                >
+                    <l-squircle 
+                        size="30" 
+                        stroke="7" 
+                        bg-opacity="0" 
+                        color="white"
+                    />
+                </div>
+        </>
+    )
 }
